9245fc1a17f080c60233fdd1581e694eb01e29da
Nicobuh <nicas13@yandex.ru>
2020-11-04 12:19:49 +0500

    corrected

--- HW02_select/SQLQuery1.sql
+++ HW02_select/SQLQuery1.sql
@@ -1,8 +1,9 @@
 USE WideWorldImporters
 --1. Все товары, в названии которых есть "urgent" или название начинается с "Animal". Вывести: ИД товара, наименование товара.
 --Таблицы: Warehouse.StockItems.
-select StockItemID,StockItemName
-from WideWorldImporters.Warehouse.StockItems
+select
+		StockItemID,StockItemName
+from Warehouse.StockItems
 where
 	StockItemName like '%urgent%' or StockItemName like 'Animal%';
 
@@ -11,9 +12,10 @@ where
 SELECT   
   s.SupplierID,
   s.SupplierName
-FROM Purchasing.Suppliers s
-Left JOIN Purchasing.PurchaseOrders t 
-	ON t.SupplierID = s.SupplierID
+FROM 
+			Purchasing.Suppliers s
+	Left JOIN Purchasing.PurchaseOrders t 
+					ON t.SupplierID = s.SupplierID
 WHERE 
 	t.PurchaseOrderID is null
 ORDER BY s.SupplierID;
@@ -30,18 +32,19 @@ ORDER BY s.SupplierID;
 --Таблицы: Sales.Orders, Sales.OrderLines, Sales.Customers.
 select s.OrderID,format(s.OrderDate,'dd.MM.yyyy') as Дата, month(s.OrderDate) as Месяц,
 		datepart(qq,s.OrderDate) as Квартал,
-		(case 
-		when  month(s.OrderDate) between '1' and '4' then '1'
+		/*(case 
+		when  month(s.OrderDate) between '1' and '4' then '1'		-- Альтернативное решение
 		when  month(s.OrderDate) between '5' and '8' then '2'
-		when  month(s.OrderDate) between '9' and '12' then '3'
-		end) as Треть_года,
+		else '3'
+		end)*/
+		ceiling (convert(float, month(s.orderdate))/4) as Треть_года,
 		t.UnitPrice,t.Quantity,t.PickingCompletedWhen, z.CustomerName
 from Sales.Orders s, Sales.OrderLines t, Sales.Customers z
 where
-s.OrderID=t.OrderID and
-z.CustomerID=s.CustomerID and
-(UnitPrice>100 or Quantity>20)
-and t.PickingCompletedWhen is not null
+	s.OrderID=t.OrderID and
+	z.CustomerID=s.CustomerID and
+	(UnitPrice>100 or Quantity>20)and
+	t.PickingCompletedWhen is not null
 order by Квартал, Треть_года, Дата  --сортировка записей
 offset 1000 rows fetch first 100 rows only; --постраничная выгрузка
 
@@ -53,26 +56,36 @@ offset 1000 rows fetch first 100 rows only; --постраничная выгрузка
 --* имя поставщика
 --* имя контактного лица принимавшего заказ (ContactPerson)
 --Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
-SET DATEFORMAT mdy;
 select 
-c.DeliveryMethodName as [способ доставки],b.ExpectedDeliveryDate as [дата доставки],
-a.SupplierName as [имя поставщика],d.FullName as [имя контактного лица]
+	c.DeliveryMethodName as [способ доставки],
+	b.ExpectedDeliveryDate as [дата доставки],
+	a.SupplierName as [имя поставщика],
+	d.FullName as [имя контактного лица]
 from 
-Purchasing.Suppliers a, Purchasing.PurchaseOrders b, Application.DeliveryMethods c, Application.People d
+		Purchasing.Suppliers a,
+		Purchasing.PurchaseOrders b,
+		Application.DeliveryMethods c,
+		Application.People d
 where
-c.DeliveryMethodID=b.DeliveryMethodID and
-b.ContactPersonID=d.PersonID and
-a.SupplierID=b.SupplierID and
-DeliveryMethodName in ('Air Freight','Refrigerated Air Freight') 
-and ExpectedDeliveryDate between '01.01.2014' and '01.01.2014'
+	c.DeliveryMethodID=b.DeliveryMethodID and
+	b.ContactPersonID=d.PersonID and
+	a.SupplierID=b.SupplierID and
+	DeliveryMethodName in ('Air Freight','Refrigerated Air Freight') and
+	ExpectedDeliveryDate between '01.01.2014' and '31.01.2014'
 Order by [дата доставки]; 
 
 
 
 --5. Десять последних продаж (по дате) с именем клиента и именем сотрудника, который оформил заказ (SalespersonPerson).
-select top(10) a.InvoiceDate as [Дата продажи], b.FullName as [Имя клиента],c.FullName as [Имя сотрудника]
+select top(10) a.InvoiceDate as [Дата продажи],
+					b.FullName as [Имя клиента],
+					c.FullName as [Имя сотрудника]
 from 
-Sales.Invoices a inner join Application.People b  on AccountsPersonID=PersonID inner join Application.People c on SalespersonPersonID=c.PersonID
+		Sales.Invoices a
+		inner join Application.People b
+				on AccountsPersonID=PersonID
+		inner join Application.People c
+				on SalespersonPersonID=c.PersonID
 Order by InvoiceDate desc;
 
 
@@ -80,9 +93,13 @@ Order by InvoiceDate desc;
 --6. Все ид и имена клиентов и их контактные телефоны, которые покупали товар Chocolate frogs 250g. Имя товара смотреть в Warehouse.StockItems.
 select distinct a.PersonID, a.FullName, a.PhoneNumber
 from 
-Application.People a join Sales.Invoices b on b.AccountsPersonID=a.PersonID
-					 join Warehouse.StockItemTransactions c  on b.InvoiceID=c.InvoiceID
-					 join Warehouse.StockItems d on d.StockItemID=c.StockItemID and d.StockItemName in ('Chocolate frogs 250g')
+				Application.People a
+			join Sales.Invoices b
+					on b.AccountsPersonID=a.PersonID
+			join Warehouse.StockItemTransactions c
+					on b.InvoiceID=c.InvoiceID
+			join Warehouse.StockItems d
+					on d.StockItemID=c.StockItemID and d.StockItemName in ('Chocolate frogs 250g')
 Order by a.PersonID;
 
 
@@ -95,8 +112,16 @@ Order by a.PersonID;
 --* Общая сумма продаж
 
 --Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
-select   year(b.InvoiceDate) as [Год продажи],month(b.InvoiceDate) as [Месяц продажи],avg(a.UnitPrice)as[Средняя цена],sum(a.UnitPrice) as [Общая сумма]
-from Sales.OrderLines a join Sales.Invoices b on a.OrderID=b.OrderID
+select   
+		year(b.InvoiceDate) as [Год продажи],
+		month(b.InvoiceDate) as [Месяц продажи],
+		avg(a.UnitPrice)as[Средняя цена],
+		sum(a.Quantity) as [Количество],
+		sum(a.UnitPrice) as [Общая стоимость]
+from
+			Sales.OrderLines a 
+		join Sales.Invoices b 
+					on a.OrderID=b.OrderID
 Group by year(b.InvoiceDate),month(b.InvoiceDate)
 Order by year(b.InvoiceDate),month(b.InvoiceDate);
 
@@ -109,8 +134,15 @@ Order by year(b.InvoiceDate),month(b.InvoiceDate);
 --* Общая сумма продаж
 
 --Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
-select   year(b.InvoiceDate) as [Год продажи],month(b.InvoiceDate) as [Месяц продажи],sum(a.UnitPrice) as [Общая сумма продаж]
-from Sales.OrderLines a join Sales.Invoices b on a.OrderID=b.OrderID
+select 
+		year(b.InvoiceDate) as [Год продажи],
+		month(b.InvoiceDate) as [Месяц продажи],
+		sum(a.Quantity) as [Количество],
+		sum(a.UnitPrice) as [Общая стоимость]
+from
+			Sales.OrderLines a
+		join Sales.Invoices b
+					on a.OrderID=b.OrderID
 Group by year(b.InvoiceDate),month(b.InvoiceDate)
 Having sum(a.UnitPrice)>10000
 Order by year(b.InvoiceDate),month(b.InvoiceDate)
@@ -132,7 +164,10 @@ select   year(b.InvoiceDate) as [Год продажи],
 			a.Description as [Наименование товара],
 			min(b.InvoiceDate) as [Дата первой продажи],
 			sum(a.Quantity) as [Количество проданного]
-from Sales.OrderLines a join Sales.Invoices b on a.OrderID=b.OrderID
+from 
+			Sales.OrderLines a
+			join Sales.Invoices b
+						on a.OrderID=b.OrderID
 Group by year(b.InvoiceDate),month(b.InvoiceDate), a.Description
 Having sum(a.Quantity)<50
 Order by year(b.InvoiceDate),month(b.InvoiceDate), a.Description
